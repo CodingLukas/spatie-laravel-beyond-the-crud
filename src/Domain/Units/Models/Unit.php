023cd4bcac8e86b7eb9362f07f5887de53e1d31c
Lukas <lukasimbm@gmail.com>
2021-07-06 15:54:12 +0300

    lesson 1-3 structure, dto

---
+++ src/Domain/Units/Models/Unit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain\Units\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model
+{
+    use HasFactory;
+}
